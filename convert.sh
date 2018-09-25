949c514ac8a487f9af2be516bca5500c934748cf
itchy <richard.h.styron@gmail.com>
2018-09-25 14:08:58 -0700

    updated conversion script

--- convert.sh
+++ convert.sh
@@ -1,6 +1,11 @@
-rm shapefile/central_am_caribbean.*
-ogr2ogr -f "ESRI Shapefile" shapefile/central_am_caribbean.shp sqlite/central_am_caribbean.sqlite 
-rm geojson/central_am_caribbean.geojson
-ogr2ogr -f "GeoJSON" geojson/central_am_caribbean.geojson sqlite/central_am_caribbean.sqlite 
-ogr2ogr -f "GMT" gmt/central_am_caribbean.gmt sqlite/central_am_caribbean.sqlite
-ogr2ogr -f "KML" kml/central_am_caribbean.kml sqlite/central_am_caribbean.sqlite
+rm shapefile/central_am_caribbean_faults.*
+ogr2ogr -f "ESRI Shapefile" shapefile/central_am_caribbean_faults.shp geojson/central_am_caribbean_faults.geojson 
+
+rm geopackage/*.*
+ogr2ogr -f "GPKG" geopackage/central_am_caribbean_faults.gpkg geojson/central_am_caribbean_faults.geojson
+
+rm GMT/*.*
+ogr2ogr -f "GMT" geopackage/central_am_caribbean_faults.gpkg geojson/central_am_caribbean_faults.geojson
+
+rm KML/*.*
+ogr2ogr -f "KML" geopackage/central_am_caribbean_faults.gpkg geojson/central_am_caribbean_faults.geojson
