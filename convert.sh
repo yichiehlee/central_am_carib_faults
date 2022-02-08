3b1860da5eb050d1b483254456cd61c3c90b365e
Jeremy Maurer <maurer.jeremy@gmail.com>
2022-02-07 19:04:55 -0600

    Update fault model files

--- convert.sh
+++ convert.sh
@@ -1,11 +1,12 @@
 rm shapefile/central_am_caribbean_faults.*
 ogr2ogr -f "ESRI Shapefile" shapefile/central_am_caribbean_faults.shp geojson/central_am_caribbean_faults.geojson 
 
-rm geopackage/*.*
-ogr2ogr -f "GPKG" geopackage/central_am_caribbean_faults.gpkg geojson/central_am_caribbean_faults.geojson
-
 rm gmt/*.*
 ogr2ogr -f "GMT" gmt/central_am_caribbean_faults.gmt geojson/central_am_caribbean_faults.geojson
 
 rm kml/*.*
 ogr2ogr -f "KML" kml/central_am_caribbean_faults.kml geojson/central_am_caribbean_faults.geojson
+
+rm geopackage/*.*
+ogr2ogr -f "GPKG" geopackage/central_am_caribbean_faults.gpkg geojson/central_am_caribbean_faults.geojson
+
